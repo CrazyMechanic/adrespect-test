fa1e57727ea0ee33b5206fef3819bdd5e5e18127
CrazyMechanic <egortimchenko94@gmail.com>
2023-08-21 00:17:32 +0300

    end carousel for projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,11 @@
+let slides = document.getElementsByClassName('slide__item');
 let container = document.querySelector('.item-container');
+let carouselInner = document.querySelector('#carousel-inner');
+let imgArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+let startingIndex = 0;
 let slideIndex = 1;
 
 let msnry = new Masonry(container, {
-  // containerStyle: 'relative',
   horizontalOrder: true,
   gutter: 43,
   columnWidth: 204,
@@ -11,20 +14,14 @@ let msnry = new Masonry(container, {
 
 showSlides(slideIndex);
 
-// Next/previous controls
+createImg();
+
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
 function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName('slide__item');
-  let dots = document.getElementsByClassName('dot');
   if (n > slides.length) {
     slideIndex = 1;
   }
@@ -34,9 +31,38 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = 'none';
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(' active', '');
-  }
   slides[slideIndex - 1].style.display = 'block';
-  dots[slideIndex - 1].className += ' active';
 }
+
+function createImg(startIndex) {
+  carouselInner.innerHTML = '';
+
+  for (let i = 0; i <= imgArray.length - 1; i++) {
+    let imgContainer = document.createElement('div');
+    let img = document.createElement('img');
+
+    imgContainer.setAttribute('class', 'carousel-item');
+    img.setAttribute('class', 'd-block w-100');
+    img.setAttribute('alt', `project-${i}`);
+    img.setAttribute('src', `assets/img/projects/Photo-${imgArray[i]}.jpg`);
+
+    if (i === startIndex) {
+      imgContainer.classList.add('active');
+    }
+
+    imgContainer.appendChild(img);
+    carouselInner.appendChild(imgContainer);
+  }
+}
+
+const itemImages = document.querySelectorAll('.item-img');
+
+itemImages.forEach((img, index) => {
+  img.dataset.bsTarget = '#exampleModal';
+  img.dataset.bsToggle = 'modal';
+
+  img.addEventListener('click', () => {
+    startingIndex = index;
+    createImg(startingIndex);
+  });
+});
